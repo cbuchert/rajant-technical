1deaa719f0684abfc4b6c29b1c117fd94236d75e
Chris <christianbuchert@gmail.com>
2023-05-23 11:30:20 -0600

    Added some simple form validation with Zod.

--- src/components/NewListItem.tsx
+++ src/components/NewListItem.tsx
@@ -1,4 +1,6 @@
-import { FC, FormEvent } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { FC } from "react"
+import { useForm } from "react-hook-form"
 import { RiAddLine } from "react-icons/ri"
 import { z } from "zod"
 import { useAppDispatch } from "../app/hooks"
@@ -6,27 +8,37 @@ import { LIST_ACTIONS } from "../features/list/list-slice"
 import styles from "./NewListItem.module.css"
 
 const NewListItem: FC = () => {
-  const schema = z.string()
-  .nonempty("Please enter a value")
-  const dispatch = useAppDispatch()
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    const data = new FormData(e.target as HTMLFormElement)
+  const schema = z.object({
+    text: z.preprocess((u: unknown) => typeof u === "string" ? u.trim() : u, z.string()
+    .nonempty("Please enter a list item.")),
+  })
+  type FormSchemaType = z.infer<typeof schema>
 
+  const { register, handleSubmit, formState: { errors } } = useForm<FormSchemaType>({
+    resolver: zodResolver(schema),
+  })
+  console.log(errors)
+
+  const dispatch = useAppDispatch()
+  const onSubmit = ({ text }: FormSchemaType) => {
     dispatch({
       type: LIST_ACTIONS.ADD_ITEM,
       payload: {
         id: new Date().toISOString(),
-        text: data.get("value") as string,
+        text,
       },
     })
   }
 
-  return <form onSubmit={handleSubmit} className={styles.container}>
-    <input className={styles.input} name={"value"} type="text"/>
+  return <form onSubmit={handleSubmit(onSubmit)} >
+    <div className={styles.container}>
+    <input className={styles.input} type="text" {...register("text")}/>
     <button
       className={"rounded px-6 py-2 bg-blue-600 hover:bg-blue-500 active:bg-blue-700 text-white hover:text-blue-50 transition-colors duration-200"}
-      type="submit"><RiAddLine className={"h-6 w-6"}/></button>
+      type="submit"><RiAddLine className={"h-6 w-6"}/>
+    </button>
+    </div>
+    <p className={"text-md text-red-400"}>{errors.text?.message}&nbsp;</p>
   </form>
 }
 
