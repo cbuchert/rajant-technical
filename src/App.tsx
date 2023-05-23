946e3123049e23a3aa291a51d5b54b524d8a33c4
Chris <christianbuchert@gmail.com>
2023-05-22 19:19:27 -0600

    Stubbed out base components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react"
+import styles from "./App.module.css"
+import List from "./components/List"
+import NewListItem from "./components/NewListItem"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className={styles.app}>
+    <h1 className={styles.heading}>List</h1>
+    <List/>
+    <NewListItem/>
+  </div>
+)
 
-export default App;
+export default App
---
+++ src/components/NewListItem.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react"
+
+const NewListItem: FC = () => {
+  return <div>New List Item</div>
+}
+
+export default NewListItem
