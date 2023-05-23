07306cb18f9fecc9711e93a9c7f6d8db3f0f9f76
Chris <christianbuchert@gmail.com>
2023-05-22 20:40:20 -0600

    New list items can be pushed to the list.

--- src/components/NewListItem.tsx
+++ src/components/NewListItem.tsx
@@ -1,7 +1,25 @@
-import { FC } from "react"
+import { FC, FormEvent } from "react"
+import { useAppDispatch } from "../app/hooks"
 
 const NewListItem: FC = () => {
-  return <div>New List Item</div>
+  const dispatch = useAppDispatch()
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    const data = new FormData(e.target as HTMLFormElement)
+
+    dispatch({
+      type: "list/addItem",
+      payload: {
+        id: new Date().toISOString(),
+        text: data.get("value") as string,
+      },
+    })
+  }
+
+  return <form onSubmit={handleSubmit}>
+    <input name={"value"} type="text"/>
+    <button type="submit">Add</button>
+  </form>
 }
 
 export default NewListItem
